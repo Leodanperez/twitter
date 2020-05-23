d29ef01632f547519bf3d6624dc9587d0cadf2fd
Leodanperez <leodanperez.1998@gmail.com>
2020-05-22 20:11:08 -0500

    verPerfil

---
+++ bd/buscoPerfil.go
@@ -0,0 +1,34 @@
+package bd
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/Leodanperez/twitter/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func BuscoPerfil(ID string) (models.Usuario, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+
+	db := MongoCN.Database("twitter")
+	col := db.Collection("usuarios")
+
+	var perfil models.Usuario
+	objID, _ := primitive.ObjectIDFromHex(ID)
+
+	condicion := bson.M{
+		"_id": objID,
+	}
+
+	err := col.FindOne(ctx, condicion).Decode(&perfil)
+	perfil.Password = ""
+	if err != nil {
+		fmt.Println("Registro no encontrado " + err.Error())
+		return perfil, err
+	}
+	return perfil, nil
+}
---
+++ routers/verPerfil.go
@@ -0,0 +1,27 @@
+package routers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Leodanperez/twitter/bd"
+)
+
+/*VerPerfil permite extraer los valores del Perfil*/
+func VerPerfil(w http.ResponseWriter, r *http.Request) {
+	ID := r.URL.Query().Get("id")
+	if len(ID) < 1 {
+		http.Error(w, "Debe enviar el parametro ID", http.StatusBadRequest)
+		return
+	}
+
+	perfil, err := bd.BuscoPerfil(ID)
+	if err != nil {
+		http.Error(w, "Ocurrio un error al intentar buscar el registro "+err.Error(), 400)
+		return
+	}
+
+	w.Header().Set("context-type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(perfil)
+}

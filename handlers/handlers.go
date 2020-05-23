15a06dfd3684bb8fdf48a243b6ca162b236a037b
Leodanperez <leodanperez.1998@gmail.com>
2020-05-22 20:35:24 -0500

    verPerfilUpdate

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -17,7 +17,7 @@ func Manejadores() {
 
 	router.HandleFunc("/registro", middlew.ChequeoBD(routers.Registro)).Methods("POST")
 	router.HandleFunc("/login", middlew.ChequeoBD(routers.Login)).Methods("POST")
-	/*router.HandleFunc("/verperfil", middlew.ChequeoBD(middlew.ValidoJWT(routers.VerPerfil))).Methods ("GET")*/
+	router.HandleFunc("/verperfil", middlew.ChequeoBD(middlew.ValidoJWT(routers.VerPerfil))).Methods("GET")
 
 	PORT := os.Getenv("PORT")
 	if PORT == "" {

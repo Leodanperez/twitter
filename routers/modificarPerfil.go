22202a83c28a746dbc654778a9fd74ad09cffc66
Leodanperez <leodanperez.1998@gmail.com>
2020-05-23 13:06:53 -0500

    leoTweets

---
+++ routers/modificarPerfil.go
@@ -0,0 +1,34 @@
+package routers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Leodanperez/twitter/bd"
+	"github.com/Leodanperez/twitter/models"
+)
+
+func ModificarPerfil(w http.ResponseWriter, r *http.Request) {
+	var t models.Usuario
+
+	err := json.NewDecoder(r.Body).Decode(&t)
+	if err != nil {
+		http.Error(w, "Datos Incorrectos "+err.Error(), 400)
+		return
+	}
+
+	var status bool
+
+	status, err = bd.ModificoRegistro(t, IDUsuario)
+	if err != nil {
+		http.Error(w, "Ocurrión un error al intentar modificar el registro. Reintente nuevamente "+err.Error(), 400)
+		return
+	}
+
+	if status == false {
+		http.Error(w, "No se ha logrado modificar el registro del usuario ", 400)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
